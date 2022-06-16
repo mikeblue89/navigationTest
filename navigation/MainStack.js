947918253b71131f1d763d96f1bbc860a4f5988e
mespinoza <michael.espinoza@forzalatam.com>
2022-06-16 08:09:34 -0600

    primer commit

---
+++ navigation/MainStack.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import ScanScreen from '../screens/ScanScreen';
+import {StyleSheet, Icon} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const MainStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Home',
+            headerStyle: styles.customHeader,
+            headerTintColor: 'white',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Scan"
+          component={ScanScreen}
+          options={{
+            title: 'Scan',
+            headerStyle: styles.customHeader,
+            headerTintColor: 'white',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  customHeader: {
+    backgroundColor: '#fc141c',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default MainStack;
---
+++ screens/Home.js
@@ -0,0 +1,32 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, TouchableOpacity, View, Image} from 'react-native';
+
+const Home = ({navigation}) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Image source={require('../assets/Multigroup.png')} />
+      <Text>Home Screen</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Scan');
+        }}
+        style={{
+          backgroundColor: 'black',
+          paddingHorizontal: 10,
+          paddingVertical: 10,
+          borderRadius: 8,
+        }}>
+        <Text style={{color: 'white'}}>Press here</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
