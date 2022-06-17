88c3336ccdfac047dcf0d2c56a064bae48f43d10
mespinoza <michael.espinoza@forzalatam.com>
2022-06-17 11:59:31 -0600

    Implementacion de vector icons

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,19 +1,22 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {RNCamera} from 'react-native-camera';
+import { RNCamera } from 'react-native-camera';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const ScanScreen = () => {
   const [name, setName] = useState('');
   const [peso, setPeso] = useState(null);
   const [loading, setLoad] = useState(true);
+  const [flash, setFlash] = useState(0);
 
   useEffect(() => {}, [loading]);
+  useEffect(() => {}, [flash]);
   const onCheckResults = e => {
     const response = JSON.parse(e.data);
-    console.log(RNCamera.Constants.FlashMode.auto)
+    console.log(RNCamera.Constants.FlashMode.auto);
     setName(response.Hola);
     setPeso(response.Heigth);
     setLoad(false);
@@ -25,39 +28,82 @@ const ScanScreen = () => {
         {
           flexDirection: 'column',
         },
-      ]}>
-      <View style={{flex: 1, alignItems: 'center', paddingHorizontal: 8}}>
+      ]}
+    >
+      <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: 8 }}>
         <Text
           style={{
             color: 'black',
             fontSize: 18,
             fontWeight: 'bold',
             paddingTop: 50,
-          }}>
+          }}
+        >
           Scan Screen
         </Text>
       </View>
-      <View style={{flex: 3}}>
-        <QRCodeScanner
-          onRead={onCheckResults}
-          flashMode={RNCamera.Constants.FlashMode.off}
-          containerStyle = {{height: 50}}
-          cameraStyle = {{height: 50}}
-        />
+      <View style={{ flex: 3 }}>
+        <QRCodeScanner onRead={onCheckResults} flashMode={flash} />
       </View>
-      <View style={{flex: 1, alignItems: 'center', paddingVertical: 8}}>
+      <View style={{ flex: 1, alignItems: 'center', paddingVertical: 8 }}>
         {loading ? (
-          <View style={{paddingTop: 50}}>
-            <Text style={{color: 'black', fontSize: 18, fontWeight: 'bold'}}>
-              Cargando ....
-            </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingTop: 50,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                backgroundColor: 'black',
+                alignItems: 'center',
+                borderRadius: 15,
+                marginHorizontal: 10,
+              }}
+              onPress={() => {
+                setFlash(1);
+              }}
+            >
+              <Icon
+                style={{
+                  color: 'white',
+                  paddingHorizontal: 6,
+                  paddingVertical: 6,
+                }}
+                name="flashlight"
+                size={20}
+                color="#4F8EF7"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: 'black',
+                alignItems: 'center',
+                borderRadius: 15,
+                marginHorizontal: 10,
+              }}
+              onPress={() => {
+                setFlash(1);
+              }}
+            >
+              <Icon
+                style={{
+                  color: 'white',
+                  paddingHorizontal: 6,
+                  paddingVertical: 6,
+                }}
+                name="cloud"
+                size={20}
+                color="#4F8EF7"
+              />
+            </TouchableOpacity>
           </View>
         ) : (
-          <View style={{paddingTop: 50}}>
-            <Text style={{color: 'black', fontSize: 18, fontWeight: 'bold'}}>
+          <View style={{ paddingTop: 50 }}>
+            <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>
               Nombre: {name}
             </Text>
-            <Text style={{color: 'black', fontSize: 18, fontWeight: 'bold'}}>
+            <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>
               Peso: {`${peso}`}
             </Text>
           </View>
